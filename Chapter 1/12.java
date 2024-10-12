b8559a42e2dac0c2cd3af320106d9c753343ec0a
桓潇潇 <104177538+hxxznb@users.noreply.github.com>
2024-10-12 09:02:52 +0800

    Update 12.java

--- Chapter 1/12.java
+++ Chapter 1/12.java
@@ -1,1 +1,9 @@
-
+public class a2 {
+    public static void main(String[] args){
+        System.out.println(" Welcome to Java");
+        System.out.println(" Welcome to Java");
+        System.out.println(" Welcome to Java");
+        System.out.println(" Welcome to Java");
+        System.out.println(" Welcome to Java");
+    }
+}
