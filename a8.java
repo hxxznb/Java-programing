f679db094867d6a57629506799e9d4aff6b7df3a
桓潇潇 <104177538+hxxznb@users.noreply.github.com>
2024-10-12 13:40:02 +0800

    Delete a8.java

--- a8.java
+++
@@ -1,9 +0,0 @@
-public class a8 {
-    public static void main(String[] args) {
-        final double PI=3.14159;
-        double r=5.5;
-        double l=2*r*PI;
-        double s=PI*r*r;
-        System.out.println("perimeter = "+l+" area = "+s);
-    }
-}
