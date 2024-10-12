94933432609f61ee4965d52470256459dcec9f4c
桓潇潇 <104177538+hxxznb@users.noreply.github.com>
2024-10-12 13:34:32 +0800

    Update b1.java

--- Chapter 2/b1.java
+++ Chapter 2/b1.java
@@ -1,1 +1,11 @@
-
+import java.util.Scanner;
+public class b1{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+        System.out.println("Enter a degree in Celsius: ");
+        double Celsius=scanner.nextDouble();  
+        double Fahrenheit=(9.0/5.0)*Celsius+32;
+        System.err.println(Celsius+" Celsius is "+Fahrenheit+" Fahrenheit");
+        scanner.close(); 
+    }
+}
