5bfcd1cdc45bb760700347f815822d90cbfb8617
桓潇潇 <104177538+hxxznb@users.noreply.github.com>
2024-11-30 23:36:24 +0800

    Add files via upload

---
+++ Week12/TestStudent.java
@@ -0,0 +1,17 @@
+
+public class TestStudent {
+    public static void main (String[] args){
+        Student student = new Student("Alice",20);
+
+        System.out.println("Name: "+student.getName());
+        System.out.println("Age: "+student.getAge());
+
+        student.setName("Bob");
+        student.setAge(25);
+
+        student.setAge(-5);
+
+        System.out.println("Name: "+student.getName());
+        System.out.println("Age: "+student.getAge());   
+    }
+}
